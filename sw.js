25ba7d65f6a09e7b889dd564400b11a5637fd38b
amal198704-hazel <amal198704@gmail.com>
2026-04-17 06:08:06 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll(['./', './index.html', './manifest.json'])
+    )
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
+self.addEventListener('message', (e) => {
+  if (e.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
